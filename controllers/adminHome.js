66a3a9276e7ecfd2d8960043cd95ab453ecbea61
Ashique Imran <imranashik50@gmail.com>
2018-03-17 01:33:12 +0600

    admin edit/delete part added

--- controllers/adminHome.js
+++ controllers/adminHome.js
@@ -6,6 +6,13 @@ var bestPlaces = require.main.require('./models/bestPlaces-model');
 var bookingPlaces = require.main.require('./models/bookingPlaces-model');
 var bestMember= require.main.require('./models/bestMember-model');
 var confirmBook= require.main.require('./models/confirmBook-model');
+var findExistAdmin = require.main.require('./models/findExistAdmin');
+var adminRegModel = require.main.require('./models/adminReg-model');
+var addNewInfo = require.main.require('./models/addNewInfo-model');
+var getAll = require.main.require('./models/ShowAllInfo-model');
+var deleteInfo = require.main.require('./models/deleteInfo-model');
+var editInfo = require.main.require('./models/editInfo-model');
+var updateInfo = require.main.require('./models/updateInfo-model');
 
 router.get('/', function(req, res){
 
@@ -74,9 +81,141 @@ router.get('/bestMember', function(req, res){
 
 router.get('/newAdmin', function(req, res){
 
-	res.render('admin/home',{msg: req.session.loggedAdmin});
+	res.render('admin/newAdmin');
 });
 
+router.post('/newAdmin', function(req, res){
 
+		var user = {
+		email: req.body.email,
+		password: req.body.password,
+		confirmPass: req.body.pass,
+		name: req.body.name,
+		addedBy: req.session.loggedAdmin
+	};
+	if(user.password!=user.confirmPass)
+	{
+		res.render('reg/index',{msg: 'confrim password did not match!'});
+	}
+	else{
+		findExistAdmin.findTheAdminExits(user, function(valid){
+			if(valid)
+			{
+				res.render('reg/index',{msg: 'Email already exist'});
+			}
+			else
+			{
+				adminRegModel.insertAdmin(user,function(flag){
+
+					if(flag!=false)
+					{
+						res.render('reg/index',{msg: 'One row inserted'});
+					}
+					else
+					{
+						res.render('reg/index',{msg: 'Error occured'});
+					}
+
+				});
+			}
+	});
+}
+});
+
+router.get('/addNewInfo', function(req, res){
+
+	res.render('admin/addNewInfo');
+});
+
+router.post('/addNewInfo', function(req, res){
+
+	var info = {
+		plcaeName: req.body.place,
+		details: req.body.details,
+		hotel: req.body.hotel,
+		category: req.body.category,
+		capacity: req.body.capacity,
+		lastEdit: req.session.loggedAdmin
+	};
+	//console.log(info);
+	addNewInfo.add(info,function(result)
+		{
+			if(result != false)
+			{
+				res.render('admin/addNewInfo',{msg: 'Data added'});
+			}
+			else
+			{
+				res.render('admin/addNewInfo',{msg: 'Sorry!! could not add'});
+			}
+		});
+});
+
+router.get('/showAll',function(req,res){
+
+	getAll.allInfo(function(result){
+		if(result != false)
+		{
+			//console.log(result);
+			res.render('admin/showAll',{result: result});
+		}
+		else
+		{
+			res.render('admin/showAll',{msg: 'something went wrong!!'});
+		}
+	});
+});
+
+router.get('/showAll/del/:id',function(req,res){
+		deleteInfo.deleteRow(req.params.id,function(value){
+		if(value)
+		{
+			//console.log(result);
+			res.redirect('/adminHome/showAll');
+		}
+		else
+		{
+			res.render('admin/home',{msg: 'Sorry!! could not delete'});
+		}
+	});
+});
+
+router.get('/showAll/edit/:id',function(req,res){
+		editInfo.editRow(req.params.id,function(result){
+		if(result != null)
+		{
+			//console.log(result);
+			res.render('admin/editRow',{result:result});
+		}
+		else
+		{
+			res.render('admin/editRow',{msg: 'Sorry!! could not load data'});
+		}
+	});
+});
+
+router.post('/showAll/edit/:id',function(req,res){
+
+		var info = {
+		plcaeName: req.body.place,
+		details: req.body.details,
+		hotel: req.body.hotel,
+		category: req.body.category,
+		capacity: req.body.capacity,
+		lastEdit: req.session.loggedAdmin
+		};
+
+		updateInfo.updateRow(req.params.id,info,function(result){
+		if(result != null)
+		{
+			//console.log(result);
+			res.redirect('/adminHome/showAll');
+		}
+		else
+		{
+			res.render('admin/home',{msg: 'Sorry!! could not upadte the row'});
+		}
+	});
+});
 
 module.exports = router;
